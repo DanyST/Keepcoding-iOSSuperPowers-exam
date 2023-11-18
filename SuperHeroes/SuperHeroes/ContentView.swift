71b5209abb27efee3586785cf37e3c3d4485d5db
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-18 18:29:07 -0300

    Add project and .gitignore

---
+++ SuperHeroes/SuperHeroes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperHeroes
+//
+//  Created by Luis Eduardo Herrera Lillo on 18-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperHeroes/SuperHeroes/SuperHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesApp.swift
+//  SuperHeroes
+//
+//  Created by Luis Eduardo Herrera Lillo on 18-11-23.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
