063380e4c3ed22371526fd82001d364e937a7986
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-19 16:40:39 -0300

    Add getSeries method and refactor HeroesNetwork

--- SuperHeroes/SuperHeroes/Data/Network/HeroesNetwork.swift
+++ SuperHeroes/SuperHeroes/Data/Network/HeroesNetwork.swift
@@ -3,30 +3,44 @@ import Combine
 
 struct HeroesNetwork: HeroesNetworkProtocol {
     private let networkProvider: NetworkProviderProtocol
-    private let heroContainerDTOToDomainMapper: HeroContainerDTOToDomainMapper
+    private let heroContainerDTOToDomainMapper: HeroContainerDTOToDomainMapperProtocol
+    private let serieContainerDTOToDomainMapper: SerieContainerDTOToDomainMapperProtocol
     
     init(
         networkProvider: NetworkProviderProtocol = NetworkProvider(),
-        heroContainerDTOToDomainMapper: HeroContainerDTOToDomainMapper = HeroContainerDTOToDomainMapper()
+        heroContainerDTOToDomainMapper: HeroContainerDTOToDomainMapperProtocol = HeroContainerDTOToDomainMapper(),
+        serieContainerDTOToDomainMapper: SerieContainerDTOToDomainMapperProtocol = SerieContainerDTOToDomainMapper()
     ) {
         self.networkProvider = networkProvider
         self.heroContainerDTOToDomainMapper = heroContainerDTOToDomainMapper
+        self.serieContainerDTOToDomainMapper = serieContainerDTOToDomainMapper
     }
         
     func getHeroes() -> AnyPublisher<ModelDataContainer<Hero>, ApiError> {
-        let params: [String: Any] = [
-            "apikey": ApiConfig.apikey,
-            "hash": ApiConfig.hash,
-            "ts": ApiConfig.ts
-        ]
+        let params: [String: Any] = ApiConfig.toDictionary
         
         return networkProvider.request(
             of: ModelDataWrapperDTO<HeroDTO>.self,
             from: .heroes(params: params),
             additionalHeaders: nil
         )
-        .map {modelDataWrapperDTO in
+        .map { modelDataWrapperDTO in
             self.heroContainerDTOToDomainMapper.map(modelDataWrapperDTO.data)
+        }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+    }
+    
+    func getSeries(by heroId: String) -> AnyPublisher<ModelDataContainer<Serie>, ApiError> {
+        let params: [String: Any] = ApiConfig.toDictionary
+        
+        return networkProvider.request(
+            of: ModelDataWrapperDTO<SerieDTO>.self,
+            from: .series(heroId: heroId, additionalParams: params),
+            additionalHeaders: nil
+        )
+        .map { modelDataWrapperDTO in
+            self.serieContainerDTOToDomainMapper.map(modelDataWrapperDTO.data)
         }
         .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
