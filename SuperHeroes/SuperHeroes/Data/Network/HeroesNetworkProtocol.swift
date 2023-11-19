ca1af3590fb36314ad064762c0f14d6e6eb43332
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-19 16:42:29 -0300

    Add getSeries method to HeroesNetworkProtocol

--- SuperHeroes/SuperHeroes/Data/Network/HeroesNetworkProtocol.swift
+++ SuperHeroes/SuperHeroes/Data/Network/HeroesNetworkProtocol.swift
@@ -3,4 +3,5 @@ import Combine
 
 protocol HeroesNetworkProtocol {
     func getHeroes() -> AnyPublisher<ModelDataContainer<Hero>, ApiError>
+    func getSeries(by heroId: String) -> AnyPublisher<ModelDataContainer<Serie>, ApiError>
 }
