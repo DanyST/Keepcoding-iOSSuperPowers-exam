c2c486ace228edb56b38fc068531f26b7e56bd24
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-18 22:49:18 -0300

    Add ApiConfig

---
+++ SuperHeroes/SuperHeroes/Data/ApiConfig.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+// Read Marvel Api Documentation for complete hash, ts and apikey info
+// https://developer.marvel.com/documentation/authorization
+enum ApiConfig {
+    static let hash = ""
+    static let ts = ""
+    static let apikey = ""
+}
