c8577f367ffdc4444fed46faaf46099c7cbe0084
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-19 17:06:14 -0300

    Add HeroDetailViewModel

---
+++ SuperHeroes/SuperHeroes/Presentation/HeroDetail/HeroDetailViewModel.swift
@@ -0,0 +1,36 @@
+import Foundation
+import Combine
+
+final class HeroDetailViewModel: ObservableObject {
+    private let getSeriesByHeroUseCase: GetSeriesByHeroUseCaseProtocol
+    private var suscriptors = Set<AnyCancellable>()
+    
+    @Published var status: Status = .none
+    @Published var series = [Serie]()
+    @Published private(set) var hero: Hero
+    
+    init(
+        hero: Hero,
+        getSeriesByHeroUseCase: GetSeriesByHeroUseCaseProtocol = GetSeriesByHeroUseCase()
+    ) {
+        self._hero = Published(initialValue: hero)
+        self.getSeriesByHeroUseCase = getSeriesByHeroUseCase
+        getSeries(by: hero.id)
+    }
+    
+    func getSeries(by heroId: String) {
+        getSeriesByHeroUseCase.execute(heroId: heroId)
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    self?.status = .loaded
+                case let .failure(error):
+                    print("Error: ", error)
+                    self?.status = .error(error: "")
+                }
+            } receiveValue: { [weak self] series in
+                self?.series = series
+            }
+            .store(in: &suscriptors)
+    }
+}
