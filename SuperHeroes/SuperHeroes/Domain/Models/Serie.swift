52d9fb600b12c5f9956607748636ba514cdd3f94
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-19 15:50:46 -0300

    Add Serie domain model

---
+++ SuperHeroes/SuperHeroes/Domain/Models/Serie.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Serie {
+    let id: Int
+    let title: String
+    let description: String
+    let startYear: Int?
+    let endYear: Int?
+    let rating: String?
+    let type: String?
+    let modified: String?
+    let thumbnail: String?
+}
