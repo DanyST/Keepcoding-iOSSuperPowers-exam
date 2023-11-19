aa27029e2c3347ed4e4d00be7eed0fe6f5d49000
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-11-19 00:40:00 -0300

    Add HeroListViewModel

---
+++ SuperHeroes/SuperHeroes/Presentation/HeroList/HeroListViewModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Combine
+
+final class HeroListViewModel: ObservableObject {
+    private let getHeroesUseCase: GetHeroesUseCaseProtocol
+    private var suscriptors = Set<AnyCancellable>()
+    
+    @Published var status: Status = .none
+    @Published var heroes = [Hero]()
+    
+    init(getHeroesUseCase: GetHeroesUseCaseProtocol = GetHeroesUseCase()) {
+        self.getHeroesUseCase = getHeroesUseCase
+        getHeroes()
+    }
+    
+    func getHeroes() {
+        status = .loading
+        
+        getHeroesUseCase.execute()
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    self?.status = .loaded
+                case let .failure(error):
+                    print("Error: ", error)
+                    self?.status = .error(error: "")
+                }
+            } receiveValue: { [weak self] heroes in
+                self?.heroes = heroes
+            }
+            .store(in: &suscriptors)
+    }
+}
